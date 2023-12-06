4bd4e1929c2bacb877e541aef2363e22b08a33ad
Raed Amri <raed.amri@esprit.tn>
2023-12-06 11:02:31 +0100

    Prosit_2

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,21 +1,37 @@
-import java.util.Scanner;
-
 public class ZooManagement {
-    int nbrCages = 20;
-    String zooName = "myZoo";
 
     public static void main(String[] args) {
-        //Instruction 1
-        ZooManagement zm = new ZooManagement();
-        System.out.println(zm.zooName + " comporte " + zm.nbrCages);
+        //PROSIT 2
+        Animal lion = new Animal();
+        lion.name = "Simba";
+        lion.age = 8;
+        lion.family = "Cats";
+        lion.isMammal = true;
 
-        System.out.println("-------------------------------------------------------------");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Veuillez saisir le nombre de cages souhaité:");
-        int nbrCagesSaisie = sc.nextInt();
-        ZooManagement zm1 = new ZooManagement();
-        zm1.nbrCages = nbrCagesSaisie;
-        zm1.zooName = "notMyZoo";
-        System.out.println(zm1.zooName + " comporte " + zm1.nbrCages);
+        Zoo myZoo = new Zoo();
+        myZoo.name = "Wildlife Park";
+        myZoo.city = "Ariana";
+        myZoo.nbrCages = 25;
+        myZoo.animals = new Animal[25];
+
+
+        Animal dog = new Animal("Canine", "Snoopy", 2, true);
+
+        System.out.println(myZoo);
+        System.out.println(myZoo.toString());
+
+        System.out.println(myZoo.addAnimal(lion));
+        System.out.println(myZoo.addAnimal(dog));
+
+        myZoo.displayAnimals();
+
+        System.out.println(myZoo.searchAnimal(dog));
+        Animal dog2 = new Animal("Canine", "Snoopy", 2, true);
+        System.out.println(myZoo.searchAnimal(dog2));
+
+        System.out.println(myZoo.removeAnimal(dog));
+        myZoo.displayAnimals();
+
     }
+
 }
