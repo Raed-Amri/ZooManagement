834298a94880b13f7f6b648d0fe121ba4316059e
Raed Amri <raed.amri@esprit.tn>
2023-12-06 10:45:29 +0100

    Prosit

---
+++ src/ZooManagement.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ZooManagement {
+    int nbrCages = 20;
+    String zooName = "myZoo";
+
+    public static void main(String[] args) {
+        //Instruction 1
+        ZooManagement zm = new ZooManagement();
+        System.out.println(zm.zooName + " comporte " + zm.nbrCages);
+
+        System.out.println("-------------------------------------------------------------");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez saisir le nombre de cages souhaité:");
+        int nbrCagesSaisie = sc.nextInt();
+        ZooManagement zm1 = new ZooManagement();
+        zm1.nbrCages = nbrCagesSaisie;
+        zm1.zooName = "notMyZoo";
+        System.out.println(zm1.zooName + " comporte " + zm1.nbrCages);
+    }
+}
