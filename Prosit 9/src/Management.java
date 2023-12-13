13b93da1020492aaecd92ea1e98460dc5b20b8a4
Raed Amri <raed.amri@esprit.tn>
2023-12-13 23:21:28 +0100

    Prosit_12

---
+++ Prosit 9/src/Management.java
@@ -0,0 +1,18 @@
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.Comparator;
+import java.util.stream.Stream;
+public interface Management  {
+    void displayStudents(List<Etudient> etudiant, Consumer<Etudient> con);
+    void displayStudentsByFilter(List<Etudient> etudiant, Predicate<Etudient> pre, Consumer<Etudient> con);
+    String returnStudentsNames(List<Etudient> etudiant, Function<Etudient, String> fun);
+    Etudient createStudent(Supplier<Etudient> sup);
+    List<Etudient> sortStudentsById(List<Etudient> etudiant, Comparator<Etudient> com);
+
+    Stream<Etudient> convertToStream(List<Etudient> etudiant);
+}
+
